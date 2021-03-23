543af390f900c3145d7477b57c16dfc802122081
Gabriel <gabst138@gmail.com>
2021-03-23 12:34:14 -0300

    Update in exercise

--- world challenges 3/Listas(Arrays) 2/df89.py
+++ world challenges 3/Listas(Arrays) 2/df89.py
@@ -1,8 +1,17 @@
-main_list = [[], [], []]
+main_list = [[], []]
 response = 'S'
 
 while response == 'S':
     name = str(input('Nome: '))
     note1 = float(input('Nota 1: '))
     note2 = float(input('Nota 2: '))
-    response = str(input('Quer continuar? [S/N] '))[0].upper()
+    response = str(input('Quer continuar? [S/N]: '))[0].upper()
+    main_list[0].append(name)
+    main_list[1].append(note1)
+    main_list[1].append(note2)
+    media = sum(main_list[1]) / len(main_list[1]) # sum --> the sum of all items divided by the size of the array
+    for k, v in enumerate(main_list[0]):
+        print(20 * '-=')
+        print(f'NO.  NOME       MEDIA')
+        print(f'{k}    {v}     {media}')
+        print(15 * '-')
