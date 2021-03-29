68caed5cc8738e8805d664d4aba4030087ff78e8
Gabriel <gabst138@gmail.com>
2021-03-29 20:31:44 -0300

    commit in df91.py
    upgrade exercise

--- world challenges 3/dictionary/df91.py
+++ world challenges 3/dictionary/df91.py
@@ -1,12 +1,19 @@
 from random import randint
 from time import sleep
+from operator import itemgetter
 players = {
-    'jogador 1': randint(1, 6),
-    'jogador 2': randint(1, 6),
-    'jogador 3': randint(1, 6),
-    'jogador 4': randint(1, 6)
+    # parte 0     parte 1
+    'jogador1': randint(1, 6),
+    'jogador2': randint(1, 6),
+    'jogador3': randint(1, 6),
+    'jogador4': randint(1, 6)
 }
+ranking = list()
 for k, v in players.items():
+    print(f'O {k} tirou {v} no dado')
     sleep(1)
-    print(f'O {k} tirou {v}')
-    sleep(1.2)
+ranking = sorted(players.items(), key=itemgetter(1), reverse=True)
+print('  ======= Ranking =======')
+for i, values in enumerate(ranking):
+    print(f'  {i+1}° lugar: {values[0]} com {values[1]}')
+    sleep(1)
