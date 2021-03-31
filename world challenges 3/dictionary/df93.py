98bf0a75f81bcd54e5e594aa6ddd934f6a94a6d8
Gabriel <gabst138@gmail.com>
2021-03-30 22:46:50 -0300

    upgrade exe 93

--- world challenges 3/dictionary/df93.py
+++ world challenges 3/dictionary/df93.py
@@ -4,9 +4,8 @@ goals = []
 main_dict['name'] = str(input('Nome do jogador: '))
 match = int(input(f'Quantas partidas {main_dict["name"]} jogou?: '))
 for c in range(1, match + 1):
-    num = int(input(f'Quantos gols na partida {c}°: '))
-    goals.append(num)
-    main_dict['goals'] = goals
+    goals.append(int(input(f'Quantos gols na partida {c}°: ')))
+    main_dict['goals'] = goals[:]
     main_dict['tot'] = sum(goals)
 print(30 * '-=')
 print(main_dict)
@@ -14,3 +13,7 @@ print(30 * '-=')
 for k, v in main_dict.items():
     print(f'O campo {k} tem o valor {v}')
 print(30 * '-=')
+print(f'O jogador {main_dict["name"]} jogou {match} partidas.')
+for key, value in enumerate(goals):
+    print(f'   => Na partida {key + 1}°, fez {value} gols.')
+print(f'Foi um total de {sum(goals)} gols.')
