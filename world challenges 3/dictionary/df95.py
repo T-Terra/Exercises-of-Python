eb7ab94af5dbc9cfea02a47a07a9ddbf1192b2fe
Gabriel <gabst138@gmail.com>
2021-03-31 22:16:28 -0300

    Create exe 95
    Create and update exe 95

---
+++ world challenges 3/dictionary/df95.py
@@ -0,0 +1,23 @@
+main_dict = dict()
+goals = []
+list_m = list()
+
+while True:
+    main_dict.clear()
+    main_dict['name'] = str(input('Nome do jogador: '))
+    match = int(input(f'Quantas partidas {main_dict["name"]} jogou?: '))
+    for c in range(1, match + 1):
+        goals.append(int(input(f'Quantos gols na partida {c}°: ')))
+        main_dict['goals'] = goals[:]
+        main_dict['tot'] = sum(goals)
+    list_m.append(main_dict.copy())
+    while True:
+        response = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if response in 'SN':
+            break
+        print('ERRO! Digite apenas S ou N.')
+    if response == 'N':
+        break
+print(30 * '-=')
+print(f'cod  nome          gols         total')
+print(30 * '-')
