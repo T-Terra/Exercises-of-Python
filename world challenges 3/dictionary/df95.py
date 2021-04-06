e1a09aa4edd083ffb24f7d9a9bff1824a1dc73a9
Gabriel <gabst138@gmail.com>
2021-04-06 15:45:43 -0300

    exe finished

--- world challenges 3/dictionary/df95.py
+++ world challenges 3/dictionary/df95.py
@@ -19,17 +19,28 @@ while True:
     if response == 'N':
         break
 print(30 * '-=')
-print(f'cod  nome          gols         total')
-print(30 * '-')
+# Faz o cabeçalho
+print(f'cod ', end='')
+for i in main_dict.keys():
+    print(f'{i:<15}', end='')
+print()
+print(40 * '-')
 for i, v in enumerate(list_m):
-    print(f' {i:<3} {v["name"]} {v["goals"]} {v["tot"]}')
-print(30 * '-')
+    print(f' {i:<3}', end='')
+    for val in v.values():
+        print(f'{str(val):<15}', end='')
+    print()
+print(40 * '-')
 while True:
     date_player = int(input('Mostrar dados de qual jogador? (999 para parar): '))
-    print(f'  -- LEVANTAMENTO DO JOGADOR {v["name"]}: ')
-    if date_player == i:
-        for cont in range(1, match + 1):
-            print(f'  No jogo {cont} fez {} gols.')
     if date_player == 999:
         break
+    if date_player >= len(list_m):
+        print(f'ERRO! Não existe jogador com o código {date_player}.')
+    else:
+        print(f'  -- LEVANTAMENTO DO JOGADOR {v["name"]}: ')
+        for i, g in enumerate(list_m[date_player]["goals"]):
+            print(f'   No jogo {i+1}° fez {g} gols.')
+        print(40 * '-')
+print('Volte sempre!')
 # Fazer a opção de ver dados de jogadores separadamente
