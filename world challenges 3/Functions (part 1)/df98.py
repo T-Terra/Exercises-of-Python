60f9fd04fa248f7d5ba3af8d016e0e33e2cc6d67
Gabriel <gabst138@gmail.com>
2021-04-08 11:17:51 -0300

    Update df98.py

--- world challenges 3/Functions (part 1)/df98.py
+++ world challenges 3/Functions (part 1)/df98.py
@@ -1,20 +1,35 @@
 from time import sleep
 
-def cont_c():
+def cont_c(i, f, p):
+    if p < 0:
+        p *= -1 # for numbers negative||
+    if p == 0:
+        p = 1
     print(20 * '-=')
-    print('Contagem de 1 até 10 de 1 em 1')
-    for c in range(1, 11):
-        sleep(0.5)
-        print(f'{c}', end=' ')
-    print('FIM!')
-    print(20 * '-=')
-    print('Contagem de 10 até 0 de 2 em 2')
-    for d in range(10, 0 - 1, -2):
-        sleep(0.5)
-        print(f'{d}', end=' ')
-    print('FIM!')
-    print(20 * '-=')
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    if i < f:
+        cont = i
+        while cont <= f:
+            sleep(0.5)
+            print(f'{cont}', end=' ')
+            cont += p
+        print('FIM!')
+    else:
+        cont = i
+        while cont >= f:
+            sleep(0.5)
+            print(f'{cont}', end=' ')
+            cont -= p
+        print('FIM!')
+print(20 * '-=')
 
 
 # main program
-cont_c()
+cont_c(1, 10, 1)
+cont_c(10, 0, 2)
+print(20 * '-=')
+print('Agora é sua vez de personalizar a contagem!')
+init = int(input('Início: '))
+finish = int(input('Fim: '))
+step = int(input('Passo: '))
+cont_c(init, finish, step)
