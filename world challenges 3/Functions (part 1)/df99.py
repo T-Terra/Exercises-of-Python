3b105f70744e8bc241449fea2a5c66baf0bc8fd4
Gabriel <gabst138@gmail.com>
2021-04-10 13:56:09 -0300

    Update df99.py

--- world challenges 3/Functions (part 1)/df99.py
+++ world challenges 3/Functions (part 1)/df99.py
@@ -2,12 +2,19 @@ from time import sleep
 
 def bigger(*num):
     tam = len(num)
-    print(num)
+    max_n = max(num)
+    for c in num:
+        sleep(0.5)
+        print(f'{c}', end=' ')
+    if tam == max_n:
+        
     print(f'Foram informados {tam} valores ao todo.')
+    print(f'O maior número informado foi {max_n}')
+    print(30 * '-=')
         
 # main program
+print(30 * '-=')
 print('Analisando os valores passados...')
-sleep(1)
 bigger(2, 9, 4, 5, 7, 1)
-sleep(1)
-bigger(2, 9)
+bigger(2, 5, 11)
+bigger()
