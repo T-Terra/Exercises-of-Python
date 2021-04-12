53425e9b48aedf0c6cfb017b2bdf08ce2f220e7c
Gabriel <gabst138@gmail.com>
2021-04-11 23:27:19 -0300

    Update df99.py

--- world challenges 3/Functions (part 1)/df99.py
+++ world challenges 3/Functions (part 1)/df99.py
@@ -6,8 +6,8 @@ def bigger(*num):
     for c in num:
         sleep(0.5)
         print(f'{c}', end=' ')
-    if tam == max_n:
-        
+    if num == 0:
+        num = 0
     print(f'Foram informados {tam} valores ao todo.')
     print(f'O maior número informado foi {max_n}')
     print(30 * '-=')
@@ -17,4 +17,4 @@ print(30 * '-=')
 print('Analisando os valores passados...')
 bigger(2, 9, 4, 5, 7, 1)
 bigger(2, 5, 11)
-bigger()
+bigger(0)
