864deaebff4a9a259370d86e2941fad6c4fd9a00
Gabriel <gabst138@gmail.com>
2021-04-12 21:26:34 -0300

    Update df100.py

--- world challenges 3/Functions (part 1)/df100.py
+++ world challenges 3/Functions (part 1)/df100.py
@@ -3,21 +3,21 @@ from time import sleep
 
 def draw():
     sum_list = []
-    par = []
-    cont = 0
+    cont = sum_tot = 0
     print(f'Sorteando 5 valores da lista: ', end='')
     while True:
         sleep(0.4)
         num = randint(0, 50)
         sum_list.append(num)
-        if num % 2 == 0 and num not in sum_list:
-            par.append(num)
+        for v in sum_list:
+            if v % 2 == 0:
+                sum_tot += v
         print(f'{num}', end=' ')
         if cont == 4:
             break
         cont += 1
     print('PRONTO!')
-    print(f'Somando os valores pares de {sum_list}, temos {sum(par)}')
+    print(f'Somando os valores pares de {sum_list}, temos {sum_tot}')
 #def sum_v():
     
 
