61e45b45a354179714aa073640690cd5a5724b04
Gabriel <gabst138@gmail.com>
2021-04-13 19:41:25 -0300

    Update df100.py

--- world challenges 3/Functions (part 1)/df100.py
+++ world challenges 3/Functions (part 1)/df100.py
@@ -1,26 +1,24 @@
 from random import randint
 from time import sleep
-
-def draw():
-    sum_list = []
-    cont = sum_tot = 0
-    print(f'Sorteando 5 valores da lista: ', end='')
-    while True:
+# function to sorted numbers
+def numbers(mainlist):
+    print(f'Sorteando 5 valores da lista:', end=' ')
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        mainlist.append(n)
+        print(f'{n}', end=' ')
         sleep(0.4)
-        num = randint(0, 50)
-        sum_list.append(num)
-        for v in sum_list:
-            if v % 2 == 0:
-                sum_tot += v
-        print(f'{num}', end=' ')
-        if cont == 4:
-            break
-        cont += 1
     print('PRONTO!')
-    print(f'Somando os valores pares de {sum_list}, temos {sum_tot}')
-#def sum_v():
-    
+
+# function to sum 
+def sum_tot(mainlist):
+    sum_finish = 0
+    for value in main_list:
+        if value % 2 == 0:
+            sum_finish += value
+    print(f'Somando os valores pares de {main_list} , temos {sum_finish}')
 
 # main program
-draw()
-#sum_v()
+main_list = []
+numbers(main_list)
+sum_tot(main_list)
