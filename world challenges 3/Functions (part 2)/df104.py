395352d3d0f2f5ebbc766d0a2759e84d02d4d5dd
Gabriel <gabst138@gmail.com>
2021-04-19 14:33:45 -0300

    Create df104.py

---
+++ world challenges 3/Functions (part 2)/df104.py
@@ -0,0 +1,13 @@
+def readint(n=0):
+    n = str(input(n))
+    if n.isnumeric():
+        return n
+    while n.strip() == '' or n.isalpha():
+        print('\33[31m''ERRO! Digite um número válido.''\33[0m')
+        n = str(input('Digite um número: '))
+    return n
+
+
+# main program
+numb = readint('Digite um número: ')
+print(f'Você acabou de digitar o número {numb}')
