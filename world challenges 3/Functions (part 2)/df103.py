edb51fab5c162bb70290f8707344bed80471f829
Gabriel <gabst138@gmail.com>
2021-04-19 13:49:36 -0300

    Update df103.py

--- world challenges 3/Functions (part 2)/df103.py
+++ world challenges 3/Functions (part 2)/df103.py
@@ -1,14 +1,14 @@
-def player_record(name='', num_g=''):
-    if name == '' and num_g == '':
-        return f'O jogador <desconhecido> fez 0 gol(s) no campeonato'
-    elif name == '' and len(num_g) > 0:
-        return f'O jogador <desconhecido> fez {num_g} gol(s) no campeonato.'
-    elif len(name) > 0 and len(num_g) == 0:
-        return f'O jogador {name} fez 0 gol(s) no campeonato'
-    else:
-        return f'O jogador {name} fez {num_g} gol(s) no campeonato.'
+def player_record(name='<desconhecido>', num_g=0):
+    print(f'O jogador {name} fez {num_g} gol(s) no campeonato.')
 
 # main program
 name_player = str(input('Nome do jogador: '))
 num_goals = str(input('Número de gols: '))
-print(player_record(name_player, num_goals))
+if num_goals.isnumeric():
+    num_goals = int(num_goals)
+else:
+    num_goals = 0
+if name_player.strip() == '':
+    player_record(num_g=num_goals)
+else:
+    player_record(name_player, num_goals)
