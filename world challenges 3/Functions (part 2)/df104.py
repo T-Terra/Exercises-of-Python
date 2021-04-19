17feea47c82182154568244cffc9a7c6cbd486f1
Gabriel <gabst138@gmail.com>
2021-04-19 15:00:29 -0300

    Update df104.py

--- world challenges 3/Functions (part 2)/df104.py
+++ world challenges 3/Functions (part 2)/df104.py
@@ -1,12 +1,30 @@
-def readint(n=0):
+## old solution ##
+"""def readint(n=0):
     print(30 * '-')
     n = str(input(n))
     if n.isnumeric():
         return n
-    while n.strip() == '' or n.isalpha():
+    while n.strip() == '' or n.isalnum():
         print('\33[31m''ERRO! Digite um número válido.''\33[0m')
         n = str(input('Digite um número: '))
-    return n
+        if n.isnumeric():
+            return n"""
+## old solution ##
+
+def readint(msg):
+    ok = False
+    value = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            value = int(n)
+            ok = True
+        else:
+            print('\33[31m''ERRO! Digite um número válido.''\33[0m')
+        if ok:
+            break
+    return value
+
 
 
 # main program
