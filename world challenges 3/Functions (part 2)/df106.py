74871d24b09b33fa2f74765ab6b0f91c9858afd1
Gabriel <gabst138@gmail.com>
2021-04-20 14:52:56 -0300

    Update df106.py

--- world challenges 3/Functions (part 2)/df106.py
+++ world challenges 3/Functions (part 2)/df106.py
@@ -1,19 +1,37 @@
-def write(msg):
+from time import sleep
+c = ('\033[m', # 0 - sem cor
+    '\033[0;30;41m', # 1 - vermelho
+    '\033[0;30;42m', # 2 - verde
+    '\033[0;30;43m', # 3 - amarelo
+    '\033[0;30;44m', # 4 - azul
+    '\033[0;30;45m', # 5 - roxo
+    '\033[7;30m'     # 6 - branco
+);
+
+def terminal(com):
+    sleep(1)
+    write(f'Acessando o manual do comando \'{com}\'', 4)
+    sleep(1.5)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+
+def write(msg, color=0):
     tam = len(msg) + 4
+    print(c[color], end='')
     print(tam * '~')
     print(f'  {msg}')
     print(tam * '~')
-def terminal():
-    from time import sleep
-    write('Sistema de ajuda PYhelp')
-    while True:
-        response = str(input('Função ou Biblioteca > '))
-        if response != 'fim':
-            sleep(1)
-            write(f'Acessando o manual do comando "{response}"')
-            sleep(1.5)
-        else:
-            break
+    print(c[0], end='')
+    
 
 # main program
-terminal()
+response = ''
+while True:
+    write('Sistema de ajuda PYhelp', 3)
+    response = str(input('Função ou Biblioteca > '))
+    if response.upper() == 'FIM':
+        break
+    else:
+        terminal(response)
+write('Até logo!!', 1)
