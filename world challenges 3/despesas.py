68ecea9562e98786ab4ae052c9555725ed0804d9
Gabriel <gabst138@gmail.com>
2021-04-22 14:49:05 -0300

    Update despesas.py

--- world challenges 3/despesas.py
+++ world challenges 3/despesas.py
@@ -1,37 +1,24 @@
-despesas = [[], [], [], []]
-despesas_var = []
-    
-salario = int(input('Qual seu salário: R$ '))
-
-''' moto é 0 
-    casa é 1
-    spotify 2 '''
-
-for c in range(0, 3):
-    d = str(input('Quais são suas despesas?: '))
-    v = float(input('Valor: R$ '))
-    if d == 'moto':
-        despesas[0].append(d)
-        despesas[0].append(v)
-    elif d == 'casa':
-        despesas[1].append(d)
-        despesas[1].append(v)
-    elif d == 'spotify':
-        despesas[2].append(d)
-        despesas[2].append(v)
-soma = despesas[0][1] + despesas[1][1] + despesas[2][1]
-sobra = salario - soma
-
-# criado um laço infinito para valores adicionais
+despesas = {}
+list_despesas = []
+soma = sobra = 0
+salario = float(input('Qual seu salário: R$ '))
 while True:
-    d_var = str(input('Quais são as despesas adicionais?: '))
-    v_var = float(input('Valor: R$ '))
-    despesas_var.append(d_var)
-    despesas_var.append(v_var)
-    r = str(input('Tem mais alguma despesa para este mês? [S/N]: ')).upper()[0]
-    if r in 'N':
+    despesas.clear()
+    d = str(input('Quais são suas despesas? '))
+    value = float(input('Valor: R$ '))
+    response = str(input('Quer continuar? [S/N] '))[0].upper()
+    despesas['despesa'] = d
+    despesas['valor'] = value
+    list_despesas.append(despesas.copy())
+    soma += value
+    sobra = salario - soma
+    if response == 'N':
         break
-print(30 * '-=')
-print(f'Seu salário é de R${salario:.2f}') 
-print(f'O Total de suas despesas são R${soma:.2f}')
-print(f'Esta é a quantia que sobra para você por mês R${sobra:.2f}')
+
+print()
+print(40 * '=')
+for k, v in enumerate(list_despesas):
+    print(f'{k+1}° --> {v}')
+print(40 * '=')
+print()
+print(f'Com um salário de R${salario:.2f} e um total de despesas de R${soma:.2f} sobra por mês R${sobra:.2f} reais.\n')
