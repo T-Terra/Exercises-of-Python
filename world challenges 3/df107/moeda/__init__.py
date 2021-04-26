7c08b81f29f3c4db779a29fe10858ee974f59682
Gabriel <gabst138@gmail.com>
2021-04-26 17:20:09 -0300

    create exe 107

---
+++ world challenges 3/df107/moeda/__init__.py
@@ -0,0 +1,13 @@
+def metade(p):
+    return p / 2
+
+def dobro(p):
+    return p * 2
+
+def aumentar(p, quant):
+    percent = p + (p*quant / 100)
+    return percent
+
+def diminuir(p, quant):
+    pcent = p - (p*quant / 100)
+    return pcent
