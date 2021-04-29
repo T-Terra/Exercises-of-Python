f96926cc2ce047bc0608e7e29cd0dbc2d22a8087
Gabriel <gabst138@gmail.com>
2021-04-29 20:10:38 -0300

    update exe 112 python

--- world challenges 3/df112/utilidadescev/dado/__init__.py
+++ world challenges 3/df112/utilidadescev/dado/__init__.py
@@ -1,5 +1,10 @@
-def readmoney(param = 0):
-    p = float(input())
-    if p == '':
-        print(f'\033[33m ERRO {param} não é um número!')
-
+def readmoney(text):
+    while True:
+        p = str(input(text)).strip()
+        if p == '':
+            print('\033[31m ERRO! "" não é um preço inválido!\033[0m')
+        elif not p.isnumeric():
+            print(f'\033[31m ERRO! {p} não é um preço inválido!\033[0m')
+        if p.isnumeric():
+            return p.isnumeric()
+            break
