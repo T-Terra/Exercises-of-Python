d0b7c2e91def5b9eeb62fe1f6594cc77ba79f3de
Gabriel <gabst138@gmail.com>
2021-05-12 16:29:25 -0300

    update exe 115

--- world challenges 3/df115/cadastro/__init__.py
+++ world challenges 3/df115/cadastro/__init__.py
@@ -2,15 +2,13 @@ from os import terminal_size
 
 
 def title(msg):
-    tam = len(msg) + 24
-    print(tam * '-')
-    print(f'{msg}'.center(tam))
-    print(tam * '-')
+    print(50 * '-')
+    print(f'{msg}'.center(50))
+    print(50 * '-')
 
 def menu(msg):
-    tam = len(msg) - 91
     print(f'{msg}')
-    print(tam * '-')
+    print(50 * '-')
 
 def cadastro():
     name = str(input('Nome: '))
@@ -19,9 +17,11 @@ def cadastro():
     with open('registros.txt', 'a') as file:
         name = file.write(f'{name} ')
         age = file.write(str(f'{age} \n'))
+def mostra_p():
     with open('registros.txt', 'r') as file:
         for content in file:
             print(content)
+
         
     #with open('registros.txt', 'r') as file:
 
--- world challenges 3/df115/df115.py
+++ world challenges 3/df115/df115.py
@@ -1,8 +1,16 @@
-from cadastro import title, menu, cadastro
+from cadastro import title, menu, cadastro, mostra_p
 
 # main program
-title('MENU PRINCIPAL')
-menu('\33[33m''1''\33[0m'' - ''\33[34m''Ver pessoas cadastradas''\33[0m'' \n''\33[33m''2''\33[0m'' - ''\33[34m''Cadastrar nova pessoa''\33[0m'' \n''\33[33m''3''\33[0m'' - ''\33[34m''Sair do sistema''\33[0m''')
-resp = int(input('Qual é sua opção: '))
-if resp == 2:
-    cadastro()
+while True:
+    title('MENU PRINCIPAL')
+    menu('\33[33m''1''\33[0m'' - ''\33[34m''Ver pessoas cadastradas''\33[0m'' \n''\33[33m''2''\33[0m'' - ''\33[34m''Cadastrar nova pessoa''\33[0m'' \n''\33[33m''3''\33[0m'' - ''\33[34m''Sair do sistema''\33[0m''')
+    resp = int(input('\33[33m''Sua opção: ''\33[0m'))
+    if resp == 1:
+        title('PESSOAS CADASTRADAS')
+        mostra_p()
+    elif resp == 2:
+        title('NOVO CADASTRO')
+        cadastro()
+    elif resp == 3:
+        title('Saindo do sistema... Até logo!')
+        break
