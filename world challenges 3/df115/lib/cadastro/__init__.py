d72e5809781c040faf3fbbbf76612dfa1d6936a3
Gabriel <gabst138@gmail.com>
2021-05-17 11:30:26 -0300

    update exe 115

--- world challenges 3/df115/lib/cadastro/__init__.py
+++ world challenges 3/df115/lib/cadastro/__init__.py
@@ -1,11 +1,31 @@
 def cadastro():
-    name = str(input('Nome: ')).strip()
+    name = str(input('Nome: '))
     age = int(input('Idade: '))
     print(f'Novo registro de {name} adicionado.')
     with open('registros.txt', 'a') as file:
         name = file.write(f'{name} ')
-        age = file.write(str(f'{age} anos\n'.strip()))
+        age = file.write(str(f'{age} anos\n'.rjust()))
+
 def mostra_p():
     with open('registros.txt', 'r') as file:
         for content in file:
-            print(f'{content}')
+            print(content)
+
+def fileexist(name):
+    try:
+        a = open(name, 'rt')
+        a.close()
+    except (FileNotFoundError):
+        return False
+    else:
+        return True
+
+
+def create_file(name):
+    try:
+        a = open(name, 'wt+')
+        a.close()
+    except:
+        print('Houve um erro na criação do arquivo!')
+    else:
+        print(f'Arquivo {name} criado com sucesso!! ')
--- world challenges 3/system.py
+++ world challenges 3/system.py
@@ -2,11 +2,16 @@ from df115.lib.interface import *
 from df115.lib.cadastro import *
 from time import sleep
 
+arq = 'registros.txt'
+if not fileexist(arq):
+    create_file(arq)
+
 # main program
 while True:
     resp = menu(['Listar pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if resp == 1:
-        title('Opção 1')
+        title('PESSOAS CADASTRADAS')
+        mostra_p()
     elif resp == 2:
         title('CADASTRANDO NOVA PESSOA')
         cadastro()
